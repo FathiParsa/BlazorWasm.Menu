e6d1b53d8838303ab9acbfff1e11c6f1b4a2a012
Parsa <parsafathi.pf.2002@gmail.com>
2025-02-10 22:51:21 -0800

    Add project files.

---
+++ BlazorWasm.Menu.UI/Features/Home/Recipe.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorWasm.Menu.UI.Features.Home
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string Originality { get; set; }
+        public int TimeInMinutes { get; set; }
+        public string TimeInString => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";   
+        public IEnumerable<Ingridients> Ingridients { get; set; } = Array.Empty<Ingridients>();
+
+    }
+
+    public class Ingridients
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

524087fcb503027bf7eedd784dcc2177c4b7abe1
Parsa <parsafathi.pf.2002@gmail.com>
2025-02-13 04:17:54 -0800

    Added Entities

---
+++ BlazorWasm.Menu.DAL/Entities/Ingridient.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorWasm.Menu.DAL.Entities
+{
+    public class Ingridient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
+
---
+++ BlazorWasm.Menu.DAL/Entities/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasm.Menu.DAL.Entities
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string Originality { get; set; }
+        public int TimeInMinutes { get; set; }
+        public List<Ingridient> Ingridients { get; set; } = new List<Ingridient>();
+    }
+}
+

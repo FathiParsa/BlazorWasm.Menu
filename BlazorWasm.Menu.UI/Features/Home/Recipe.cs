757d90681e09668a41253f77f02b847adcdd97aa
Parsa <parsafathi.pf.2002@gmail.com>
2025-02-13 04:23:43 -0800

    Buges Fixed (Nul Reference Exeption) . Styles Changed.

--- BlazorWasm.Menu.UI/Features/Home/Recipe.cs
+++ BlazorWasm.Menu.UI/Features/Home/Recipe.cs
@@ -3,20 +3,20 @@
     public class Recipe
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public double Price { get; set; }
-        public string ImageUrl { get; set; }
-        public string Originality { get; set; }
-        public int TimeInMinutes { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Originality { get; set; }
+        public int? TimeInMinutes { get; set; }
         public string TimeInString => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
-        public IEnumerable<Ingridients> Ingridients { get; set; } = Array.Empty<Ingridients>();
+        public IEnumerable<Ingridients>? Ingridients { get; set; } = Array.Empty<Ingridients>();
 
     }
 
     public class Ingridients
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
